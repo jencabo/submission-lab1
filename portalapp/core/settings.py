338d5beaa898e7d243eafd29152e2a53fb1ef3f8
Fahad Zain Jawaid <Fahad.jawaid@utoronto.ca>
2022-08-10 10:55:15 -0400

    create core

--- portalapp/core/settings.py
+++ portalapp/core/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portalapp.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portalapp.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
