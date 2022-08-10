3a132e61cc56ae2b15e99029a75e2c5879c75d2f
Fahad Zain Jawaid <Fahad.jawaid@utoronto.ca>
2022-08-10 14:45:25 -0400

    add main app and set default port to 8000 and accept all incoming cxns

--- portalapp/core/models.py
+++ portalapp/core/models.py
@@ -2,10 +2,9 @@
 # Buzzerboy Template Portal Application
 # Developed by: Fahad Zain Jawaid (fjawaid@buzzerboy.com)
 # This model file should be lite, it serves as Super classes for all 
-# sub application models.
 # -----------------------------------------------
 # Each model should have the following requirements:
-# ---- 1) Audit Trail on each record (Created, Created By, Last Updated, Last Updated By)
+# ---- 1) Audit Trail on each record (Created At, Created By, Last Updated At, Last Updated By)
 # ---- 2) Some tables will be unilingual, and other tables will be multilingual, Multlingual tables
 # ---------- will store a language key field to track what language the row is in. This way, multiple
 #----------- rows of that table can together form a language specific value stream
@@ -15,3 +14,45 @@
 # All portals will also support multilingualism, so that functionalility will be part of the core 
 # --- as well. This will include a table for SupportedLanguages
 # -----------------------------------------------
+
+#Necessary imports
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+#CORE MODELS
+class SupportedLanguages(models.Model):
+    languageKey = models.CharField(max_length=4)
+    description = models.CharField(max_length=255)
+    flagURL = models.TextField
+    flagPic = models.ImageField(null=True, blank=True, default="", upload_to="settings/flags")
+
+    def __str__(self):
+        return self.description + " (" + self.languageKey + ")"
+
+    def flag_img (self):
+        return "<img id=flag'" + self.id.__str__() + "' src='" + self.flag_url + "'/>"
+
+    @property
+    def flag_url(self):
+	    if self.flagPic and hasattr(self.flagPic, 'url'):
+		    return self.flagPic.url 
+
+
+#INHERITABLE MODELS
+class AuditableBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_add_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    id = models.AutoField(primary_key=True)
+
+    class Meta:
+        abstract = True
+
+class MutlilingualBaseModel(AuditableBaseModel):
+    language = models.ForeignKey(SupportedLanguages, on_delete=models.DO_NOTHING)
+    obj_lang_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+        unique_together = ('id', 'language',)
